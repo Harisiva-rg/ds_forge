59e59cee301e0c132eae8f0c696542d1ec1f9129
harisiva-rg <harisivarg@gmail.com>
2024-02-02 12:21:59 +0000

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='dsforge',
-    version='0.0.2',
+    version='0.0.3',
     author='Harisiva R G',
     author_email='harisivarg@gmail.com',
     description='A package to create a data science project structure',
